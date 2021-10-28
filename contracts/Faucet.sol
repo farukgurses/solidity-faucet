08af2dcb317d57fea51a89ebecd464b2d3c2cdbb
Ahmet Faruk Gürses <afarukg@gmail.com>
2021-10-28 20:42:26 +0300

    added destruction functionalilty

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,30 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Faucet{
 
+contract Owned {
     address payable owner;
 
     constructor() public {
-        owner = msg.sender;
+        owner = payable(msg.sender);
     }
 
     modifier onlyOwner{
         require(msg.sender == owner);
         _;
     }
+}
 
+contract Mortal is Owned {
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
+}
+
+contract Faucet is Mortal{
     // accept any incoming amount
     receive() external payable {}  
     
     function withdraw(uint withdraw_amount) public {
         // limit withdrawal amount
-        require(withdraw_amount <= 100000000000000000);
+        require(withdraw_amount <= 0.1 ether);
         // send the amount to the address that requested it
         payable(msg.sender).transfer(withdraw_amount);
-    }
-
-    function destroy() public onlyOwner{
-        selfdestruct(owner);
     }
 }
